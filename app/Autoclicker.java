e274e3f03b6edb480075b36ef87c485b784f4202
Jake Bringham <bringhja@dukes.jmu.edu>
2020-05-10 01:29:53 -0400

    Added the app package and the class to start the program.

---
+++ app/Autoclicker.java
@@ -0,0 +1,22 @@
+package app;
+
+import gui.AutoClicker;
+
+/**
+ * To be written.
+ * 
+ * @author Jacob Bringham
+ *
+ */
+public class Autoclicker {
+
+  /**
+   * Starts the program.
+   * 
+   * @param args Unused
+   */
+  public static void main(String[] args) {
+    new AutoClicker();
+  }
+
+}
